89619745512af07d114ceeb5a20f62800ca958f0
RThompson153 <robbyt15@gmail.com>
2021-09-03 13:47:59 -0500

    Working commit
    Adds Drag and Drop folder
    Adds Drag and Drop service

---
+++ DragAndDrop/Service.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedComponents
+{
+	internal static class Service
+	{
+		internal static DraggableItem CurrentItem;
+
+		internal static void SetCurrentItem(DraggableItem item) => CurrentItem = item;
+
+		//internal static int GetItemIndex()
+		//{
+
+		//}
+	}
+}
