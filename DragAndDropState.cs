0bfa3639fcda817f00752580b621d9c0bb52cbb9
RThompson153 <robbyt15@gmail.com>
2021-09-03 09:23:42 -0500

    Working

---
+++ DragAndDropState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedComponents
+{
+	public static class DragAndDropState
+	{
+		public static DraggableItem CurrentItem;
+
+		public static void DragStart(DraggableItem item) => CurrentItem = item;
+	}
+}
