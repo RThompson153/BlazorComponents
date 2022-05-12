daab2ec59bcbf64641545f5cd54d944154a4895c
RThompson153 <robbyt15@gmail.com>
2022-05-12 13:56:04 -0500

    Working

--- DragAndDrop/Service.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SharedComponents
-{
-	internal static class Service
-	{
-		internal static DragAndDropItem CurrentItem;
-
-		internal static void SetCurrentItem(DragAndDropItem item) => CurrentItem = item;
-	}
-}
